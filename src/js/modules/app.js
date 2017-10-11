ae46bc38b3780d7d5b63edbdb25237345fa68714
platonovAlexey <kalyawe4ka@gmail.com>
2017-10-11 18:53:24 +0300

    add adaptation, fix js open section

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
     nav:false,
     autoplay:true,
     autoplayTimeout:5000,
-    autoplayHoverPause:false,
+    autoplayHoverPause:true,
     smartSpeed:600,
     animateOut: 'fadeOut',
+    responsiveClass:true,
     responsive:{
         0:{
             items:1
@@ -28,23 +29,51 @@ $(document).ready(function() {
     var transportLeftArr = $('.transport-left-arr');
     var transportRightArr = $('.transport-right-arr');
 
+    // passengerSection.show("slow");
+    // commercialSection.hide("slow");
+    // transportLeftArr.show("slow");
+    // transportRightArr.hide("slow");
+
+    // passengerSection.hide("slow");
+    // commercialSection.show("slow");
+
+    // transportLeftArr.hide("slow");
+    // transportRightArr.show("slow");
+
     passengerBtn.click(function(event) {
         event.preventDefault();
-        passengerSection.show("slow");
-        commercialSection.hide("slow");
-
-        transportLeftArr.show("slow");
-        transportRightArr.hide("slow");
+        $(this).toggleClass('open-passenger-section');
+        if ($(this).hasClass('open-passenger-section')) {
+            passengerSection.show("slow");
+            commercialSection.hide("slow");
+            transportLeftArr.show("slow");
+            transportRightArr.hide("slow");
+        }else{
+            passengerSection.hide("slow");
+            transportLeftArr.hide("slow");
+        }
+        commercialBtn.removeClass('open-commercial-section');
     });
+
 
     commercialBtn.click(function(event) {
         event.preventDefault();
-        passengerSection.hide("slow");
-        commercialSection.show("slow");
+        $(this).toggleClass('open-commercial-section');
+        if ($(this).hasClass('open-commercial-section')) {
+            passengerSection.hide("slow");
+            commercialSection.show("slow");
 
-        transportLeftArr.hide("slow");
-        transportRightArr.show("slow");
+            transportLeftArr.hide("slow");
+            transportRightArr.show("slow");
+        }else{
+            commercialSection.hide("slow");
+            transportRightArr.hide("slow");
+        }
+
+        passengerBtn.removeClass('open-passenger-section');
+
     });
+
 
     $('.header-update__item').on('click', '.js-scroll', function(event) {
         event.preventDefault();
